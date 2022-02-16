a4ad421cc8fc14b9d398f068ab7a2004f0813010
GHNDS <gabriel.private1998@gmail.com>
2022-02-16 14:27:32 -0300

    Implementing possible moves of king
    Resumo:
    Adicionado métodos e validações para a regra de movimento do rei e a própria movimentação do rei
    
    Adições/modificações:
    +canMove
    +setValues(Usando como referência pontos cardeais)

--- Chess_Project/src/application/Program.java
+++ Chess_Project/src/application/Program.java
@@ -15,24 +15,24 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		
-			while(true) {
-			try {
-				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces());
+		while(true) {
+		try {
+			UI.clearScreen();
+			UI.printBoard(chessMatch.getPieces());
 				
-				System.out.println();
-				System.out.print("Source: ");
+			System.out.println();
+			System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
 				boolean[][] possibleMoves = chessMatch.posibleMoves(source);
-				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+			UI.clearScreen();
+			UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				
-				System.out.println();
-				System.out.print("Target: ");
+			System.out.println();
+			System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
-				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
