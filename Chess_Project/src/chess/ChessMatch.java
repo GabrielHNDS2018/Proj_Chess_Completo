3e1d4d2f780182e15d3eb45ba47cd55a19dff0f0
GHNDS <gabriel.private1998@gmail.com>
2022-02-16 14:48:38 -0300

    Switching player each turn
    Resumo:
    Implementado turno para jogadores de ambos os lados tanto Black quanto White sua respectivas peças e impedindo que mova a peça do oponente com uma exception personalizada.
    
    Adições/modificações:
    +getTurn;
    +getCurrentPlayer;
    +Exception: "The choosen piece is not yours";
    +nextTurn;
    +printMatch;
    +Substituindo printBoard por printMatch no Program;

--- Chess_Project/src/chess/ChessMatch.java
+++ Chess_Project/src/chess/ChessMatch.java
@@ -8,13 +8,25 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 	
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
 	}
 	
+	public int getTurn() {
+		return turn;
+	}
+
+	public Color getCurrentPlayer() {
+		return currentPlayer;
+	}
+
 	public ChessPiece[][] getPieces(){
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		
@@ -38,6 +50,7 @@ public class ChessMatch {
 		 validateSourcePosition(source);
 		 validateTargetPosition(source, target);
 		 Piece capturePiece = makeMove(source, target);
+		 nextTurn();
 		 return (ChessPiece)capturePiece;
 	}
 	
@@ -52,6 +65,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece in source position");
 		}
+		if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			throw new ChessException("The choosen piece is not yours");
+		}
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for this piece");
 		}
@@ -61,6 +77,11 @@ public class ChessMatch {
 		if(!board.piece(source).possibleMoves(target)) {
 			throw new ChessException("The chosen piece can't move to target position");
 		}
+	}
+	
+	private void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
